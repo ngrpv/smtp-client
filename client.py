fc4590ec545fee1903f9aa2afe725afe8c31d5d1
Nazar Garipov <nazar.garipov475@gmail.com>
2022-05-30 14:47:27 +0500

    smtp

---
+++ client.py
@@ -0,0 +1,53 @@
+import os.path
+from mimetypes import types_map
+import base64
+
+BOUNDARY = '----==--bound.53245.vla1-5245e79bed88.qloud-c.yandex.net'
+
+
+class Smtp:
+    def __init__(self, user_mail: str):
+        self._mail = user_mail
+        self._attachments = []
+
+    def attach(self, filename: str):
+        if not os.path.exists(filename):
+            raise FileExistsError(f'{filename} not exists')
+        self._attachments.append(filename)
+
+    def get_smtp_data(self, title: str, message_text: str,
+                      destination_address: str) -> str:
+
+        message = (f'From: {self._mail}\n'
+                   f'To: {destination_address}\n'
+                   f'Subject: {title} \n'
+                   'MIME-Version: 1.0 \n'
+                   f'Content-Type: multipart/mixed;\n'
+                   f'\tboundary="{BOUNDARY}"\n\n'
+                   f'--{BOUNDARY}\n'
+                   'Content-Type: text/plain; charset=utf-8\n'
+                   'Content-Transfer-Encoding: 8bit\n\n'
+
+                   f'{message_text}\n')
+        for attachment in self._attachments:
+            message += self._get_attachment_data(attachment)
+        message += f'--{BOUNDARY}--\n.\n'
+        return message
+
+    @staticmethod
+    def _get_attachment_data(filename: str) -> str:
+        type = ''
+        try:
+            type = types_map["." + filename.split(".")[-1]]
+        except KeyError:
+            raise KeyError(f'Undefined mime type for {filename}')
+        data = (f'--{BOUNDARY}\n'
+                f'Content-Disposition: attachment;\n'
+                f'\tfilename="{filename.split("/")[-1]}"\n'
+                'Content-Transfer-Encoding: base64\n'
+                f'Content-Type: {type};\n'
+                f'\tname="{filename.split("/")[-1]}"\n\n')
+        with open(filename, 'rb') as f:
+            b64_encoded = base64.b64encode(f.read())
+            data += b64_encoded.decode() + '\n'
+        return data
---
+++ main.py
@@ -0,0 +1,61 @@
+import base64
+import socket
+import ssl
+from configparser import ConfigParser
+from client import Smtp
+
+config = ConfigParser()
+with open('config.cfg', 'r', encoding='utf-8') as f:
+    config.read_file(f)
+user_data = config['UserData']
+connection_data = config['Connection']
+message = config['Message']
+HOST_ADDR = connection_data['SmtpAddress']
+PORT = connection_data.getint('Port')
+TIMEOUT = connection_data.getint('Timeout')
+
+b64_mail = base64.b64encode(user_data['Mail'].encode()).decode()
+b64_password = base64.b64encode(user_data['Password'].encode()).decode()
+
+
+def _send(sock: socket.socket, data: str) -> str:
+    sock.send(f'{data}\n'.encode())
+    return sock.recv(65535).decode()
+
+
+def _get_message():
+    smtp = Smtp(user_data['Mail'])
+    for attachment in message['Attachments'].split(', '):
+        smtp.attach(attachment)
+    text = ''
+    with open(message['MessageFile'], 'r', encoding='utf-8') as msg_f:
+        text = msg_f.read()
+    return smtp.get_smtp_data(message['Title'], text, message['Mail'])
+
+
+def log(str):
+    pass
+
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST_ADDR, PORT))
+        sock = ssl.wrap_socket(sock)
+        sock.settimeout(TIMEOUT / 1000)
+        log(sock.recv(1024).decode())
+        log(_send(sock, f'ehlo {user_data["Mail"]}'))
+        log(_send(sock, 'AUTH LOGIN'))
+        log(_send(sock, b64_mail))
+        log(_send(sock, b64_password))
+        log(_send(sock, f'MAIL FROM: {user_data["Mail"]}'))
+        log(_send(sock, f'RCPT TO: {message["Mail"]}'))
+        log(_send(sock, f'DATA'))
+        log(_send(sock, _get_message()))
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
+
